517d702081ed541acae829bfbf6db28ac686eacf
Nawaz Saqlain <86104841+shaikh06@users.noreply.github.com>
2025-10-16 16:43:25 +0530

    Delete backend directory

--- backend/src/main/java/com/catering/backend/repository/MenuItemRepository.java
+++
@@ -1,7 +0,0 @@
-package com.catering.backend.repository;
-
-import com.catering.backend.model.MenuItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MenuItemRepository extends JpaRepository<MenuItem, Long> {
-}
